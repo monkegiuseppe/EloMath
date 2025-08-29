4c85b785b4a234f7c6d37a5eec72053648f19bca
wagawaga4k <late034@gmail.com>
2025-08-29 09:39:30 +0300

    bug fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -54,7 +54,7 @@ const config: Config = {
   				'5': 'hsl(var(--chart-5))'
   			},
   			sidebar: {
-  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				background: 'hsl(var(--sidebar-background))',
   				foreground: 'hsl(var(--sidebar-foreground))',
   				primary: 'hsl(var(--sidebar-primary))',
   				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
